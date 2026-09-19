6ed6788b16b0cfaef35b6409b44ef1eefc24619b
GaoYangHeng <GaoYangHeng@users.noreply.github.com>
2026-09-19 09:31:39 +0800

    chore: remove one-off reverse-engineering test scripts

--- crack_sign/testapp/hook_min.js
+++
@@ -1,7 +0,0 @@
-console.log("[SIGN] gadget loaded ok");
-
-setTimeout(function() {
-    console.log("[SIGN] timeout ok");
-    var base = Module.findBaseAddress("libRequestEncoder.so");
-    console.log("[SIGN] libRequestEncoder base = " + base);
-}, 500);
